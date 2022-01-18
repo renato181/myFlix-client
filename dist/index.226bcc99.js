5c77a1e83a23b1f7da741582b6c3d0cbc2999698
Renato <renato_181@hotmail.com>
2022-01-18 14:45:34 -0500

    updated title/description

--- dist/index.226bcc99.js
+++ dist/index.226bcc99.js
@@ -22802,21 +22802,21 @@ class MainView extends _reactDefault.default.Component {
             movies: [
                 {
                     _id: 1,
-                    Title: 'Inception',
-                    Description: 'desc1...',
-                    ImagePath: '...'
+                    Title: 'Aliens',
+                    Description: 'Fifty-seven years after surviving an apocalyptic attack aboard her space vessel by merciless space creatures, Officer Ripley awakens from hyper-sleep and tries to warn anyone who will listen about the predators.',
+                    ImagePath: 'https://m.media-amazon.com/images/M/MV5BZGU2OGY5ZTYtMWNhYy00NjZiLWI0NjUtZmNhY2JhNDRmODU3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX140_CR0,0,140,209_AL_.jpg'
                 },
                 {
                     _id: 2,
-                    Title: 'The Shawshank Redemption',
-                    Description: 'desc2...',
-                    ImagePath: '...'
+                    Title: 'River\'s Edge',
+                    Description: 'A high school slacker commits a shocking act and proceeds to let his friends in on the secret. However, the friends\' reaction is almost as ambiguous and perplexing as the crime itself.',
+                    ImagePath: 'https://m.media-amazon.com/images/M/MV5BNzMyYzVkYjctNDliZi00Yjc0LThiODctOTc5Mzk2Njc5YzM2XkEyXkFqcGdeQXVyNTc1NTQxODI@._V1_UX140_CR0,0,140,209_AL_.jpg'
                 },
                 {
                     _id: 3,
-                    Title: 'Gladiator',
-                    Description: 'desc3...',
-                    ImagePath: '...'
+                    Title: 'Platoon',
+                    Description: 'Chris Taylor, a neophyte recruit in Vietnam, finds himself caught in a battle of wills between two sergeants, one good and the other evil. A shrewd examination of the brutality of war and the duality of man in conflict.',
+                    ImagePath: 'https://m.media-amazon.com/images/M/MV5BMzRjZjdlMjQtODVkYS00N2YzLWJlYWYtMGVlN2E5MWEwMWQzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX140_CR0,0,140,209_AL_.jpg'
                 }
             ]
         };
