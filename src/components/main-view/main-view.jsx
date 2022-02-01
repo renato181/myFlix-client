5003485c18842369c018d7a5e51cb097077c1f25
Renato <renato_181@hotmail.com>
2022-01-31 20:22:45 -0500

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,44 +1,41 @@
 import React from 'react';
-import axios from 'axios';
-
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
-  constructor() {
-    myFlix-client/src/main-view/main-view.jsx
-  }
-
-  componentDidMount(){
-    axios.get('https://movies181.herokuapp.com/movies')
-      .then(response => {
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  setSelectedMovie(newSelectedMovie) {
-    myFlix-client/src/main-view/main-view.jsx
-  }
-
- render() {
-    const { movies, selectedMovie } = this.state;
-
-    if (movies.length === 0) return <div className="main-view" />;
-
-    return (
-      <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
-         ))
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                { _id: 1, Title: 'Aliens', Description: 'Fifty-seven years after surviving an apocalyptic attack aboard her space vessel by merciless space creatures, Officer Ripley awakens from hyper-sleep and tries to warn anyone who will listen about the predators.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BZGU2OGY5ZTYtMWNhYy00NjZiLWI0NjUtZmNhY2JhNDRmODU3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX140_CR0,0,140,209_AL_.jpg' },
+                { _id: 2, Title: 'River\'s Edge', Description: 'A high school slacker commits a shocking act and proceeds to let his friends in on the secret. However, the friends\' reaction is almost as ambiguous and perplexing as the crime itself.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BNzMyYzVkYjctNDliZi00Yjc0LThiODctOTc5Mzk2Njc5YzM2XkEyXkFqcGdeQXVyNTc1NTQxODI@._V1_UX140_CR0,0,140,209_AL_.jpg' },
+                { _id: 3, Title: 'Platoon', Description: 'Chris Taylor, a neophyte recruit in Vietnam, finds himself caught in a battle of wills between two sergeants, one good and the other evil. A shrewd examination of the brutality of war and the duality of man in conflict.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMzRjZjdlMjQtODVkYS00N2YzLWJlYWYtMGVlN2E5MWEwMWQzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX140_CR0,0,140,209_AL_.jpg' }
+            ]
         }
-      </div>
-    );
-  }
+    }
+
+    setSelectedMovie(newSelectedMovie) {
+        this.setState({
+            selectedMovie: newSelectedMovie
+        });
+    }
+
+    render() {
+        const { movies, selectedMovie } = this.state;
+
+
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+        return (
+            <div className="main-view">
+                {selectedMovie
+                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                    : movies.map(movie => (
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                    ))
+                }
+            </div>
+        );
+    }
+
 }
