e0c3d910784e2617244b925e6016b9fce5375386
Renato <renato_181@hotmail.com>
2022-02-17 19:19:11 -0500

    3.5

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
-
     render() {
         const { movie, onMovieClick } = this.props;
 
-        return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+        return (
+            <Row className="main-view justify-content-md-center">
+                {selectedMovie
+                    ? (
+                        <Col md={8}>
+                            <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                        </Col>
+                    )
+                    : movies.map(movie => (
+                        <Col md={3}>
+                            <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                        </Col>
+                    ))
+                }
+            </Row>
+        );
     }
 }
 
